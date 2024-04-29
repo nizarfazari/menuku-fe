b7ed0f7d156c2b58be191d75e066e7628f64fae6
Nizar Fazari <nexsel32@gmail.com>
2024-04-29 08:52:43 +0700

    re-structure components,styles and using layout - page

---
+++ app/profil/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ProfilLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <nav>
+            <p> asdaasdas</p>
+            {children}
+        </nav>
+    )
+}
---
+++ app/profil/page.tsx
@@ -0,0 +1,6 @@
+export default function Profil() {
+    return (
+        <>
+            asdas</>
+    )
+}
